5fe6d33d197538e7752d99763d411644af56def0
Jason Jiang <jiang.jason.x@gmail.com>
2016-06-17 15:56:53 -0700

    Cut ins working but no screen filter

--- BGAnimations/ScreenGameplay toasty.lua
+++ BGAnimations/ScreenGameplay toasty.lua
@@ -1,5 +1,5 @@
 local t = Def.ActorFrame {
-	LoadActor( THEME:GetPathG("","_shigu") )..{
+	LoadActor( THEME:GetPathG("","_A_X-Alice2") )..{
 		InitCommand=cmd(x,SCREEN_RIGHT-150;y,SCREEN_BOTTOM-75;zoom,0.9;diffusealpha,0);
 		OnCommand=cmd(decelerate,0.1;diffusealpha,1;sleep,0.6;addx,20;sleep,0.04;addx,30;sleep,0.04;addx,45;sleep,0.04;addx,60;sleep,0.04;addx,80;);
 	};
